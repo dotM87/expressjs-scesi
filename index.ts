ac86415ae1e4f234ddc179430ab7128603725343
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-06-02 17:14:56 -0400

    feat(index): agregando conexión al index de la aplicación

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Express, Request, Response } from 'express';
+import sequelize from '@/configs/database.config';
 import dotenv from 'dotenv';
 
 import healthyRoute from '@/routes/healthyRoute';
@@ -14,6 +15,12 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/healthy', healthyRoute);
 app.use('/products', productRoute);
+
+sequelize.sync({ force: false }).then(() => {
+  console.log('MySQL database connected and models synced');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
